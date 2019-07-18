e42bf6d6a25c9a300127706a903379b6cd87654a
Mamzi <mohammadreza.osouli@gmail.com>
2019-07-19 00:25:32 +0430

    Updates

---
+++ precache-manifest.7678b12f2e7a192740a19520eb319f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7e70bb98b053deb1345",
+    "url": "/memory-management/static/js/runtime~main.e7e70bb9.js"
+  },
+  {
+    "revision": "b60c3ca67b858f60e85c",
+    "url": "/memory-management/static/js/main.b60c3ca6.chunk.js"
+  },
+  {
+    "revision": "c6078c5f50b45ee8d179",
+    "url": "/memory-management/static/js/1.c6078c5f.chunk.js"
+  },
+  {
+    "revision": "b60c3ca67b858f60e85c",
+    "url": "/memory-management/static/css/main.20240a06.chunk.css"
+  },
+  {
+    "revision": "c6078c5f50b45ee8d179",
+    "url": "/memory-management/static/css/1.46688111.chunk.css"
+  },
+  {
+    "revision": "c70cf7defbe073598364d37b2379ca1c",
+    "url": "/memory-management/index.html"
+  }
+];
